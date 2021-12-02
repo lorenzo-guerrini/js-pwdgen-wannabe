2ed5e031e93e6aab29ca12e81da67ddf15950727
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-02 16:21:26 +0100

    esercizio finito

--- myscript.js
+++ myscript.js
@@ -3,4 +3,8 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-let name = prompt("Qual è il tuo nome?")
+let name = prompt("Qual è il tuo nome?");
+let surname = prompt("Qual è il tuo cognome?");
+let color = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("output").innerHTML = name + surname + color + 21;
