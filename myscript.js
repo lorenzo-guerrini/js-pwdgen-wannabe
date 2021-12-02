d08789a98ccbf58100e6d35152260fd8affd4c04
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-02 16:56:06 +0100

    bonus inventato da me

--- myscript.js
+++ myscript.js
@@ -3,8 +3,11 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-let name = prompt("Qual è il tuo nome?");
-let surname = prompt("Qual è il tuo cognome?");
-let color = prompt("Qual è il tuo colore preferito?");
-
-document.getElementById("output").innerHTML = name + surname + color + 21;
+function passwordGenerator() {
+    let name = prompt("Qual è il tuo nome?");
+    let surname = prompt("Qual è il tuo cognome?");
+    let color = prompt("Qual è il tuo colore preferito?");
+    
+    document.getElementById("password-container").style.display = "block";
+    document.getElementById("output").innerHTML = name + surname + color + 21;
+}
